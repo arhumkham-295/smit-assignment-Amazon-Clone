49cd767fd4b2ae6cba4eb50439a1a814e382cdbf
Arhum-Khan <arhumtiktok0@gmail.com>
2026-05-22 11:02:50 +0500

    navbar completed with responsive

---
+++ app.js
@@ -0,0 +1,16 @@
+function showdiv(){
+    var hidediv=document.getElementById("hiddendiv")
+    hidediv.hidden=false
+}
+function hidediv(){
+    var hidediv=document.getElementById("hiddendiv")
+    hidediv.hidden=true
+}
+function showdiv2(){
+    var hidediv=document.getElementById("hiddendiv2")
+    hidediv.hidden=false
+}
+function hidediv2(){
+    var hidediv=document.getElementById("hiddendiv2")
+    hidediv.hidden=true
+}
